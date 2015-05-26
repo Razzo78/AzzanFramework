ea56aae77b60aeac4209cec4bf8dcedb7d7a354a
Rudy Azzan <rudy@azzan.it>
2015-05-26 13:57:58 +0200

    Added Net project
    Added Net project foe network functions

--- AzzanFramework/AzzanCoreUniversal/Properties/AssemblyInfo.cs
+++ AzzanFramework/AzzanCoreUniversal/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
@@ -12,16 +13,12 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("Copyright © Azzan 2015")]
 [assembly: AssemblyTrademark("www.azzan.it")]
 [assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzzanFramework/AzzanNetUniversal/Strings/Extensions/StringEx.cs
@@ -0,0 +1,38 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+#endregion
+
+namespace System.Net.Mail
+{
+	/// <summary>
+	/// Net Mail extensions
+	/// </summary>
+	public static class StringEx
+	{
+		/// <summary>
+		/// Check the validity of e-mail address
+		/// </summary>
+		/// <param name="pMailAddress">E-mail address</param>
+		/// <returns>
+		/// 	<c>true</c> if [is valid email] [the specified p TXT]; otherwise, <c>false</c>.
+		/// </returns>
+		/// <remarks>
+		/// V.1.0
+		/// </remarks>
+		public static bool IsValidEmail(this String pMailAddress)
+		{
+		    
+			bool foundMatch = false;
+
+			foundMatch = Regex.IsMatch(pMailAddress, @"\A(?:^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,10}|[0-9]{1,3})(\]?)$)\z",
+					RegexOptions.IgnoreCase);
+
+			return foundMatch;
+		}
+	}
+}
+
